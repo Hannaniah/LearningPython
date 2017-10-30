fa3b1e94a24f0c868e19a671c10bd73ea7f29a7a
Hannaniah <hannaniah@bellsouth.net>
2017-10-29 20:44:10 -0400

    My first game file/commit

---
+++ game.py
@@ -0,0 +1,10 @@
+#adventurer of knot
+
+print(" You are alive, but barely.  You feel the ache of your body as if it is in a far of distance.  Around you is utter darkness.  Your mind is shatterd and your memmory is in peices.  You have no recolection of where you are or how you got there.  Only a few shards of your identity remain. '\n")
+
+user = input("Who are you?'\n ")
+i_race = input("Of what kin?'\n (Human) (unsure) (Elf) (Dwarf) (Ogre) '\n")
+i_clas = input("What disapline do you practice'\n ")
+i_diety= input("In who'm do you believe'\n")
+
+print(f"{user}, awaken!  {user}, awaken!")
